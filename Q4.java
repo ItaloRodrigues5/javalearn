bf06fc08ba8625698b8a4b0f959fff16dd389304
Italo Rodrigues <107129381+ItaloRodrigues5@users.noreply.github.com>
2024-08-20 14:57:56 -0300

    Aprendizagem inicial
    Arquivos de aprendizagem básica da linguagem Java

---
+++ Q4.java
@@ -0,0 +1,50 @@
+//Esse código é junto com o código em Carro.java;
+
+/*
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Insira a marca do carro: ");
+    String marca = scanner.nextLine();
+
+    System.out.print("Insira o modelo do carro: ");
+    String modelo = scanner.nextLine();
+
+    System.out.print("Insira o ano do carro: ");
+    int ano = scanner.nextInt();
+
+    System.out.print("Insira o valor do carro: ");
+    double preco = scanner.nextDouble();
+
+    String valorarredondado = String.format("%.2f", preco);
+    double valorpuro = Double.parseDouble(valorarredondado);
+
+    scanner.nextLine();
+
+    System.out.print("Insira a cor do carro: ");
+    String cor = scanner.nextLine();
+
+    scanner.close();
+
+    Carro carro1;
+    carro1 = new Carro();
+    carro1.marca = marca;
+    carro1.modelo = modelo;
+    carro1.ano = ano;
+    carro1.preco = valorpuro;
+    carro1.cor = cor;
+
+    /*Carro carro2 = new Carro("Xuxa", "Sasha", 2024, 99999.99, "amarelo");*/
+/*
+    System.out.println("\nA marca do carro é: " + carro1.marca);
+    System.out.println("O modelo do carro é: " + carro1.modelo);
+    System.out.println("O ano do carro é: " + carro1.ano);
+    System.out.println("O preço do carro é: " + carro1.preco);
+    System.out.println("A cor do carro é: " + carro1.cor);
+  }
+}
+*/
