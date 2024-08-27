5eb6c01a18aac70e12e0bff6cda6107e30582025
Italo Rodrigues <107129381+ItaloRodrigues5@users.noreply.github.com>
2024-08-27 19:26:42 -0300

    Update Main.java

--- Aula 1/LoginReturn/Main.java
+++ Aula 1/LoginReturn/Main.java
@@ -1,28 +1,28 @@
 import java.util.Scanner;
 
 public class Main{
-  public static void main(String[] args){
+    public static void main(String[] args){
 
-    Scanner scanner = new Scanner(System.in);
-    
-    Pessoa pessoa1 = new Pessoa();
-    pessoa1.criarLogin();
+        Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Cadastro de Usuário\n");
+        Pessoa pessoa1 = new Pessoa();
+        pessoa1.criarLogin();
 
-    System.out.print("Digite seu nome: ");
-    pessoa1.nome = scanner.nextLine();
+        System.out.println("Cadastro de Usuário\n");
 
-    System.out.print("Digite seu nome de usuário: ");
-    pessoa1.login.podeAcessarUser(scanner.nextLine());
+        System.out.print("Digite seu nome: ");
+        pessoa1.nome = scanner.nextLine();
 
-    System.out.print("Digite sua senha de cadastro: ");
-    pessoa1.login.podeAcessarPass(scanner.nextLine());
+        System.out.print("Digite seu nome de usuário: ");
+        pessoa1.login.podeAcessarUser(scanner.nextLine());
+
+        System.out.print("Digite sua senha de cadastro: ");
+        pessoa1.login.podeAcessarPass(scanner.nextLine());
 
 
     /*
     Poderia ter feito assim tbm para compreender e separar melhor as coisas:
-    
+
     System.out.print("Digite seu nome: ");
     String nome = scanner.nextLine();
 
@@ -37,32 +37,52 @@ public class Main{
     pessoa1.login.podeAcessarPass(senha);
     */
 
-    int op;
+        int op;
 
-    do{
-      System.out.println("\nLogin de Usuário\n");
+        do{
+            System.out.println("\nLogin de Usuário\n");
 
-      System.out.print("Digite seu nome de usuário: ");
-      String user = scanner.nextLine();
-      
-      System.out.print("Digite sua senha: ");
-      String password = scanner.nextLine();
-      
-      if (pessoa1.login.validaLogin(user, password)){
-        System.out.println("\nUsuário Logado com sucesso!");
-      }
-      else{
-        System.out.println("\nUsuário ou senha inválidos!");
-      }
+            System.out.print("Digite seu nome de usuário: ");
+            String user = scanner.nextLine();
 
-      System.out.println("\nDeseja sair? Digite 1 (um)");
-      op = Integer.parseInt(scanner.nextLine());
-    
+            System.out.print("Digite sua senha: ");
+            String password = scanner.nextLine();
+
+            if (pessoa1.login.validaLogin(user, password)){
+                System.out.println("\nUsuário Logado com sucesso!");
+            }
+            else{
+                System.out.println("\nUsuário ou senha inválidos!");
+            }
+
+            System.out.println("\nDeseja sair? Digite 1 (um)");
+            op = Integer.parseInt(scanner.nextLine());
+
+        }
+
+        while(op != 1);
+
+        // Imprime o retono de toString que traz os valores inseridos em Login.
+        // Só mostra por esse method
+
+        System.out.println(pessoa1.login.toString());
+
+        System.out.println("\nDeseja alterar senha?\n");
+
+        System.out.print("Digite a nova senha: ");
+        String newSenha = scanner.nextLine();
+
+        scanner.close();
+
+        pessoa1.login.alteraSenha(newSenha);
+
+        System.out.println("\nAbaixo é o toString\n");
+
+        System.out.println(pessoa1.login.toString() + "\n");
+
+        System.out.println("Abaixo é do getUsuario\n");
+
+        System.out.println(pessoa1.login.getUsuario());
+
     }
-    
-      while(op != 1);
-    
-    scanner.close();
-    
-  }
 }
