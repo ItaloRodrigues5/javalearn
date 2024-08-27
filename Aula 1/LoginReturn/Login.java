29a542d026e75a6969040c60f5344956d40fb148
Italo Rodrigues <107129381+ItaloRodrigues5@users.noreply.github.com>
2024-08-27 19:27:09 -0300

    Update Login.java

--- Aula 1/LoginReturn/Login.java
+++ Aula 1/LoginReturn/Login.java
@@ -1,19 +1,61 @@
 public class Login{
-  private String usuario;
-  private String senha;
+    private String usuario;
+    private String senha;
 
-  public boolean validaLogin(String user, String password){
-    if (usuario.equals(user) && senha.equals(password)){
-      return true;
+    public void Login(String usuario, String senha){
+        this.usuario = usuario;
+        this.senha = senha;
     }
-    else{
-      return false;
+
+    public boolean validaLogin(String user, String password){
+        if (this.usuario.equals(user) && this.senha.equals(password)){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
-  }
-  public void podeAcessarUser(String username){
-    usuario = username;
-  }
-  public void podeAcessarPass(String chave){
-    senha = chave;
-  }
+    public void podeAcessarUser(String username){
+        this.usuario = username;
+    }
+    public void podeAcessarPass(String chave){
+        this.senha = chave;
+    }
+
+    public void verificaSenha(String senha, String novasenha){
+        if(this.validaSenha(senha)){
+            this.alteraSenha(novasenha);
+        }
+    }
+    public boolean validaSenha(String senha){
+        if (senha.equals(this.senha)){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public void exibirDados(){
+        System.out.println(this.senha);
+    }
+
+    public String toString(){
+        return "=====Login=====\n\nUsuário: " + usuario + "\nSenha: " + senha;
+    }
+
+    public void alteraUsuario(String usuario){
+        this.usuario = usuario;
+    }
+
+    public void alteraSenha(String senha){
+        this.senha = senha;
+    }
+
+    public String setUsuario(String usuario){
+        return this.usuario = usuario;
+    }
+    public String getUsuario(){
+        return this.usuario;
+    }
 }
