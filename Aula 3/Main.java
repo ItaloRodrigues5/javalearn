a45b8de97f27b15efb70f00846931df907607296
Italo Rodrigues <107129381+ItaloRodrigues5@users.noreply.github.com>
2024-08-28 19:31:17 -0300

    Update Main.java

--- Aula 3/Main.java
+++ Aula 3/Main.java
@@ -1,5 +1,16 @@
 public class Main{
     public static void main(String[] args) {
-        int vetor[] = new int [20];
+        int vetor[] = new int [10];
+        int cont = 0;
+        
+        for (int i = 0; i < 3; i++){
+            vetor[i] = cont++; 
+            //Realizar a soma antes da operação: ++cont;
+            //Realizar a soma depois da operação: cont++;
+        }
+
+        for(int i = 0; i < 3; i++){
+            System.out.println(vetor[i]);
+        }
     }
 }
