270958720ebf1d54d3b0b0c88985e00ac4e35cdb
Italo Rodrigues <107129381+ItaloRodrigues5@users.noreply.github.com>
2024-08-28 18:38:12 -0300

    Update Main.java

--- Exercício-1/Questão1/Main.java
+++ Exercício-1/Questão1/Main.java
@@ -13,5 +13,8 @@ public class Main{
         int soma = numero1 + numero2;
 
         System.out.println("A soma do número " + numero1 + " e " + numero2 + " = " + soma);
+
+        System.out.println("Digite 1 para Multiplicar os números\nDigite 2 para Dividir o primeiro número pelo segundo\nDigite 3 para subtrair o segundo código");
+
     }
 }
