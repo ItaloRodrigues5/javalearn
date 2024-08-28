8f0d2f326a58704eb3585e29453b761ea7ff5563
Italo Rodrigues <107129381+ItaloRodrigues5@users.noreply.github.com>
2024-08-28 19:26:08 -0300

    Update Main.java

--- Exercício-1/Questão1/Main.java
+++ Exercício-1/Questão1/Main.java
@@ -10,11 +10,44 @@ public class Main{
         System.out.print("Digite outro número inteiro: ");
         int numero2 = scanner.nextInt();
 
-        int soma = numero1 + numero2;
+        //System.out.println("A soma do número " + numero1 + " e " + numero2 + " = " + soma);
 
-        System.out.println("A soma do número " + numero1 + " e " + numero2 + " = " + soma);
+        System.out.println("Digite 1 para Somar os números\n" +
+                "Digite 2 para subtrarir o primeiro número pelo segundo\n" +
+                "Digite 3 para multiplicar o segundo código\n" +
+                "Digite 4 para dividir");
 
-        System.out.println("Digite 1 para Multiplicar os números\nDigite 2 para Dividir o primeiro número pelo segundo\nDigite 3 para subtrair o segundo código");
+        int op = scanner.nextInt();
 
+        switch(op){
+            case 1:
+                int soma = numero1 + numero2;
+                System.out.println("A soma dos números " + numero1 + " e " + numero2 + " = " + soma);
+                break;
+            case 2:
+                int subtracao = numero1 - numero2;
+                System.out.println("A subtração dos números " + numero1 + " e " + numero2 + " = " + subtracao);
+                break;
+            case 3:
+                int multiplicacao = numero1 * numero2;
+                System.out.println("A multiplicação dos números " + numero1 + " e " + numero2 + " = " + multiplicacao);
+                break;
+            case 4:
+
+                while(numero1 == 0) {
+
+                System.out.println("Digite um valor apropriado para a divisão");
+                numero1 = scanner.nextInt();
+                    if (numero1 != 0){
+                        int divisao = numero2 / numero1;
+                        System.out.println("A divisão dos números " + numero1 + " e " + numero2 + " = " + divisao);
+
+                    }
+                    else{
+                        System.out.println("Olha usuário. O divisor é 0. Logo é impossível dividir quaisquer números.");
+                    }
+                }
+                break;
+        }
     }
 }
