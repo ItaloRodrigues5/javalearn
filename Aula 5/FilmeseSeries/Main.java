e860d626447dcddbd9ccba39f07ade4237776f05
Italo Rodrigues <107129381+ItaloRodrigues5@users.noreply.github.com>
2024-09-04 19:45:48 -0300

    Create Main.java

---
+++ Aula 5/FilmeseSeries/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        Listagem listagem = new Listagem();
+
+        System.out.println("Digite o nome e o studio do seu filme");
+        listagem.setSeries(add.scannerNextLine());
+        
+        String studio = scanner.nextLine();
+
+        System.out.println("Digite o nome e o studio da sua série");
+        String nome1 = scanner.nextLine();
+        String studio1 = scanner.nextLine();
+    }
+}
