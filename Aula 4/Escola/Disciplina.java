a330fbe1aa4317aefbc828cad069e68b6af23f84
Italo Rodrigues <107129381+ItaloRodrigues5@users.noreply.github.com>
2024-09-03 21:21:24 -0300

    Create Disciplina.java

---
+++ Aula 4/Escola/Disciplina.java
@@ -0,0 +1,18 @@
+public class Disciplina {
+    private String nome;
+    private Aluno aluno;
+    private Professor professor;
+
+    public Disciplina() {
+        this.nome = nome;
+
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+}
