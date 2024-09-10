49bcd9f467722479b3bb5c70e2e6d31f55c0460e
Italo Rodrigues <107129381+ItaloRodrigues5@users.noreply.github.com>
2024-09-10 20:56:32 -0300

    Create Carrinho.java]

---
+++ Aula 6/Carrinho.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Carrinho {
+    ArrayList<Produto> produto = new ArrayList<>();
+    double total;
+
+    public Carrinho(double total){
+        this.total = total;
+    }
+
+    
+}
