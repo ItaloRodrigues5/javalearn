18f38092e949aabfa129f24eb1a4bf54a3e866f3
Italo Rodrigues <107129381+ItaloRodrigues5@users.noreply.github.com>
2024-09-10 21:00:58 -0300

    Update Loja.java

--- Aula 6/Loja.java
+++ Aula 6/Loja.java
@@ -1,27 +1,43 @@
+package com.company;
+
 import java.util.ArrayList;
 
 public class Loja {
-    ArrayList<Produto> produto = new ArrayList<>();
-    String nome;
 
-    public Loja(String nome){
+    private ArrayList<Produto> produtos = new ArrayList<>();
+    private String nome;
+
+    public Loja(String nome) {
         this.nome = nome;
     }
 
-    public void adicionaProduto(Produto produto){
-        this.produto.add(produto);
+    public Produto buscaProduto(Produto produto) {
+        if (produtos.contains(produto)){
+            System.out.println("Produto encontrado");
+            System.out.println(produto);
+            return produto;
+        }else{
+            return null;
+        }
     }
 
     public void removeProduto(Produto produto){
-        this.produto.remove(produto);
+        this.produtos.remove(produto);
     }
 
-    public boolean buscaProduto(){
-        if(this.produto.contains(produto)){
-            return true;
-        }
-        else{
-            return false;
-        }
+    public void adicionaProduto(Produto produto) {
+        this.produtos.add(produto);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public ArrayList<Produto> getProdutos() {
+        return produtos;
     }
 }
