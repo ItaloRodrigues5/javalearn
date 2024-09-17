7e4b882b41c1c5707b05f2729d9695827f7377e5
Italo Rodrigues <107129381+ItaloRodrigues5@users.noreply.github.com>
2024-09-17 20:44:26 -0300

    Update Main.java

--- Aula 8/Main.java
+++ Aula 8/Main.java
@@ -5,9 +5,13 @@ public class Main{
         Scanner scanner = new Scanner(System.in);
         
         Pessoa pessoa1 = new Pessoa();
+        Autor autor1 = new Autor();
+        Leitor leitor1 = new Leitor();
+        Biblioteca bib1 = new Biblioteca();
+        Livro livro1 = new Livro();
         
         
         System.out.print("Digite seu nome");
-        pessoa1.nome = scanner.nextLine();
+        autor1.pessoa1.nome = scanner.nextLine();
     }
 }
