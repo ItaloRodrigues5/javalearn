3add82cc677d3cb45af1b52c69a1a042b7e8f6ea
Italo Rodrigues <107129381+ItaloRodrigues5@users.noreply.github.com>
2024-09-18 16:38:20 -0300

    Create Estudante.java

---
+++ Aula 8/EstudanteTurma/Estudante.java
@@ -0,0 +1,11 @@
+public class Estudante extends Pessoa {
+    private String curso;
+    
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+    
+    public String getCurso() {
+        return curso;
+    }
+}
